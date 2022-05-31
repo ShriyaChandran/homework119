e57097b48c498305f6cae83fd209f19806843a62
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2022-05-31 19:04:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.tree import export_graphviz
+from io import StringIO 
+from IPython.display import Image
+import pydotplus
+
+col_names = ['PassengerId', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'label']
+
+df = pd.read_csv("titanic.csv", names=col_names).iloc[1:]
+
+print(df.head())
+
+features = ['PassengerId', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch']
+X = df[features]
+y = df.label
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+clf = DecisionTreeClassifier()
+clf = clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+dot_data = StringIO()
+export_graphviz(clf, out_file=dot_data, filled = True, rounded = True, special_characters = True, feature_names = features, class_names = ['0', '1'])
+print(dot_data.getvalue())
+
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png('titanic.png')
+Image(graph.create_png())
+
+
